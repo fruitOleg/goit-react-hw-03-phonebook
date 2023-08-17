eaa0771259076522f2c769e27bac3ebc57528e8e
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-08-17 10:42:24 +0200

    Initial commit

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,69 @@
+import { Component } from 'react';
+import { Field, Formik, Form, ErrorMessage } from 'formik';
+import { nanoid } from 'nanoid';
+import * as Yup from 'yup';
+const schema = Yup.object().shape({
+  name: Yup.string().min(2, 'Too Short!'),
+  number: Yup.number(),
+});
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value, id: nanoid() });
+  };
+
+  handleSubmitForm = (value, actions) => {
+    this.props.addContact(this.state);
+    actions.resetForm();
+    console.log(value);
+  };
+
+  render() {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={this.handleSubmitForm}
+      >
+        <Form>
+          <label>
+            Name
+            <Field
+              type="text"
+              name="name"
+              placeholder="Input name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <ErrorMessage name="name" />
+          </label>
+
+          <label>
+            Number
+            <Field
+              type="tel"
+              name="number"
+              placeholder="Input number"
+              value={this.state.number}
+              onChange={this.handleChange}
+            />
+            <ErrorMessage name="number" />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </Form>
+      </Formik>
+    );
+  }
+}
